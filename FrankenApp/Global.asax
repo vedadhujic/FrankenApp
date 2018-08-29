d551e09af9e5903913b5082250cf6341ed722aad
Vedad Hujić <hujicvedad@outlook.com>
2018-08-30 00:32:09 +0200

    First commit.

---
+++ FrankenApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FrankenApp.MvcApplication" Language="C#" %>
---
+++ FrankenApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrankenApp.Startup))]
+namespace FrankenApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
