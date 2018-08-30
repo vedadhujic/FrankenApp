cc3863616996861c1da3b0ceacd773f7b1fa7954
Vedad Hujić <hujicvedad@outlook.com>
2018-08-30 21:43:34 +0200

    update 1

---
+++ FrankenApp/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using FrankenApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrankenApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            User user = new User() { Id = 1, Name = "Vedad", Number = 12 };
+
+            return View(user);
+        }
+    }
+}
